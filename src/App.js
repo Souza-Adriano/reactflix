92effccd8db57c26f288610aed7238c9154df148
Adriano Souza <azuos.adriano@gmail.com>
2020-07-27 13:37:49 -0300

    :art: fim da aula 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import Menu from './components/Menu';
+import Carousel from './components/Carousel';
+import BannerMain from './components/BannerMain'
+import Footer from './components/Footer'
+import mock from './data/dados_iniciais.json';
 
 function App() {
   return (
-    <div>
+    <div style={{ backgroundColor: '#141414' }}>
       <Menu />
-      Hello world
+      <BannerMain
+        videoTitle={mock.categorias[0].videos[0].titulo}
+        url={mock.categorias[0].videos[0].url}
+        videoDescription={`Descricao do video aqui.`}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={mock.categorias[0]}
+      />
+      <Carousel
+        category={mock.categorias[1]}
+      />
+      <Carousel
+        category={mock.categorias[2]}
+      />
+      <Carousel
+        category={mock.categorias[3]}
+      />
+      <Carousel
+        category={mock.categorias[4]}
+      />
+      <Carousel
+        category={mock.categorias[5]}
+      />
+      <Footer/>
     </div>
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import './Menu.css';
+import ButtonLink from '../Template/ButtonLink';
 
 const Menu = () => (
-    <nav>
-        DocFlix Show !
+    <nav className="Menu">
+        <a href="/">
+            <img className="Logo" src={Logo} alt="LOGO" />
+        </a>
+
+        <ButtonLink className="ButtonLink" href="/">
+            Novo Video
+        </ButtonLink>
     </nav>
 );
 
